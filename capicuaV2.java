acb9fe7b0d2ad39b28dab33beb0de7b7b248c460
Nacholoff <jose.burgosloez@tiernoparla.es>
2022-10-07 19:35:57 +0200

    ARREGLADO CAPICUAV2

--- capicuaV2.java
+++ capicuaV2.java
@@ -1,17 +1,18 @@
 public class capicuaV2 {
     public static void main(String[] args) {
         
-        int numero=12345;
+        int numero=545;
         int temporal=numero;
+        int cifra;
+        int reverso = 0;
 
-        while (temporal<0) {
-            int cifra;
-            int reverso = 0;
+        while (temporal>0) {
+        
             cifra = temporal%10;
             reverso= reverso*10 + cifra;
             temporal=temporal/10;
         }
-        if (temporal==numero) {
+        if (reverso==numero) {
             System.out.println("Es Capicua");
         } else {
             System.out.println("No es capicua");
