fb94756da81f9e810a313348dce784d68aa76ccf
Nacholoff <jose.burgosloez@tiernoparla.es>
2022-10-14 16:39:36 +0200

    modificados capicua y contarvocales

--- capicuaV2.java
+++ capicuaV2.java
@@ -1,14 +1,6 @@
 public class capicuaV2 {
     public static void main(String[] args) {
         
-<<<<<<< HEAD
-        int numero=12345;
-        int temporal=numero;
-
-        while (temporal<0) {
-            int cifra;
-            int reverso = 0;
-=======
         int numero=545;
         int temporal=numero;
         int cifra;
@@ -16,16 +8,11 @@ public class capicuaV2 {
 
         while (temporal>0) {
         
->>>>>>> acb9fe7b0d2ad39b28dab33beb0de7b7b248c460
             cifra = temporal%10;
             reverso= reverso*10 + cifra;
             temporal=temporal/10;
         }
-<<<<<<< HEAD
-        if (temporal==numero) {
-=======
         if (reverso==numero) {
->>>>>>> acb9fe7b0d2ad39b28dab33beb0de7b7b248c460
             System.out.println("Es Capicua");
         } else {
             System.out.println("No es capicua");
--- contarVocales.java
+++ contarVocales.java
@@ -5,7 +5,7 @@ public class contarVocales {
     int vocales = 0;
 
     for (int i = 0; i < letras.length(); i++) {
-      if ((letras.charAt(i) == 'a')
+      if ((letras.charAt(i) == 'a')               //.length es el reccorido de uns string o in, .charAT te transforma strings en CHAR.
           || (letras.charAt(i) == 'e')
           || (letras.charAt(i) == 'i')
           || (letras.charAt(i) == 'o')
